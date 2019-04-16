072ec8f4f3b82e5b61bfd6670449f9cc64ef96df
cengland99 <47651725+cengland99@users.noreply.github.com>
2019-04-16 09:56:27 -0500

    Fixed email validation

--- main.py
+++ main.py
@@ -9,92 +9,88 @@ app.config['DEBUG'] = True
 
 @app.route("/") 
 def index():
-   return render_template("form_1.html")
+   return render_template("signup.html")
 
 
 @app.route("/validate_user", methods=["POST"])
 def validate_user():
-    username_from_form = request.form["username"]
-    password_from_form = request.form["password"]
-    verify_from_form = request.form["verify"]
-    email_from_form = request.form["email"]
+    username = request.form["username"]
+    password = request.form["password"]
+    verify = request.form["verify"]
+    email = request.form["email"]
 
-    username_logic_error = ""
-    password_logic_error = ""
-    verify_logic_error = ""
-    email_logic_error = ""
+    username_error = ""
+    password_error = ""
+    verify_error = ""
+    email_error = ""
     
-    #username logic
-    if username_from_form == "":
-        username_logic_error="Username must be populated"
+    if username == "":
+        username_error="Username must be populated"
 
-    if len(username_from_form) < 3:
-        username_logic_error = "User name must be longer than 3 and less than 20 characters" 
+    if len(username) < 3:
+        username_error = "User name must be longer than 3 and less than 20 characters" 
     
-    if len(username_from_form) >20:
-        username_logic_error = "User name must be longer than 3 and less than 20 characters"
+    if len(username) >20:
+        username_error = "User name must be longer than 3 and less than 20 characters"
     
-    if " " in username_from_form:
-        username_logic_error = "Spaces are not allowed in the username" 
+    if " " in username:
+        username_error = "Spaces are not allowed in the username" 
+
+    if password == "":
+        password_error = "Password must be populated"
     
-    #password logic
-    if password_from_form == "":
-        password_logic_error = "Password must be populated"
+    if " " in password:
+        password_error = "Spaces are not allowed in the password"
     
-    if " " in password_from_form:
-        password_logic_error = "Spaces are not allowed in the password"
+    if len(password) < 3:
+        password_error = "User name must be longer than 3 and less than 20 characters" 
     
-    if len(password_from_form) < 3:
-        password_logic_error = "User name must be longer than 3 and less than 20 characters" 
-    
-    if len(password_from_form) >20:
-        password_logic_error = "User name must be longer than 3 and less than 20 characters"
-    
-    #verify field logic
-    if verify_from_form =="":
-        verify_logic_error = "Please enter password verification"
+    if len(password) >20:
+        password_error = "User name must be longer than 3 and less than 20 characters"
+
+    if verify =="":
+        verify_error = "Please enter password verification"
      
-    if " " in verify_from_form:
-        verify_logic_error = "Spaces are not allowed in the password"    
+    if " " in verify:
+        verify_error = "Spaces are not allowed in the password"    
     
-    if not password_from_form == verify_from_form:
-        verify_logic_error = "Passwords don't match"
+    if not password == verify:
+        verify_error = "Passwords don't match"
 
-    # email logic
-    # space or not text handeling
+    if not email =="":    
  
-    if " " in email_from_form:
-        email_logic_error = "Spaces are not allowed in the email"       
-   
-    # length of email and format (requires 1 "@" and 1 ".") 
-    if len(email_from_form) <3:
-        email_logic_error = "Email must be longer than 3 and less than 20 characters" 
-    
-    if len(email_from_form) >20:
-        email_logic_error = "Email must be longer than 3 and less than 20 characters"    
-    
-    for char in email_from_form:
-        if email_from_form.count ("@") < 1:
-            email_logic_error = "The email must contain (1) @ and (1) . to be valid"
+        if " " in email:
+            email_error = "Spaces are not allowed in the email"       
 
-        elif email_from_form.count ("@") > 1:
-            email_logic_error = "The email must contain (1) @ and (1) . to be valid"
-        
-        else:
-            email_logic_error=""
 
-    all_error_messages_combined = (username_logic_error + password_logic_error + verify_logic_error + email_logic_error)
+        if len(email) <3:
+            email_error = "Email must be longer than 3 and less than 20 characters" 
+
+        if len(email) >20:
+            email_error = "Email must be longer than 3 and less than 20 characters"    
+
+        for char in email:
+            if email.count ("@") < 1:
+                email_error = "The email must contain (1) @ and (1) . to be valid"
+
+            elif email.count ("@") > 1:
+                email_error = "The email must contain (1) @ and (1) . to be valid"
+            
+            else:
+                email_error=" "
+
+    all_error_messages_combined = (username_error + password_error + verify_error + email_error)
     if all_error_messages_combined == "":
-        return render_template("hello_greeting.html",
-            username=username_from_form)
+        return render_template("welcome.html",
+            username=username)
     else:
-        return render_template("form_1.html", 
-            username=username_from_form, 
-            email=email_from_form, 
-            email_error=email_logic_error,
-            username_error=username_logic_error, 
-            password_error=password_logic_error,
-            verify_error=verify_logic_error)
+        return render_template("signup.html", 
+            username=username, 
+            email=email, 
+            email_error=email_error,
+            username_error=username_error, 
+            password_error=password_error,
+            verify_error=verify_error)
 
 app.run()
 
